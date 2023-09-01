bc3ae824fb48ac33fc1ea0c85ea64216b858ab3f
Doug Hubbard <doug@dougis.com>
2023-09-01 07:53:36 -0700

    consolidated to a single file for tests

--- server/tests/utils/user_auth.test.js
+++ server/tests/utils/user_auth.test.js
@@ -1,9 +1,18 @@
+const jwt = require("jsonwebtoken");
 const sinon = require("sinon");
+const bcrypt = require("bcrypt");
+const isEmpty = require("is-empty");
 const validation = require("../../utils/validation"); // import the validation module
 const assert = require("assert");
 const User = require("../../models/User");
-const { createUser } = require("../../utils/user_auth"); // import the createUser function
+const {
+  createUser,
+  confirmUserLogin,
+  createLoginTokenAndLogin,
+} = require("../../utils/user_auth"); // import the
+// Please note that you will need to import other necessary dependencies/mock data for successful testing
 
+// Tests for the createUser function
 describe("createUser", () => {
   let validateUserSignupStub;
   let saveStub;
@@ -51,5 +60,92 @@ describe("createUser", () => {
     assert.deepEqual(result.createErrors, {});
     assert.strictEqual(result.isValid, true);
     assert.strictEqual(typeof result.newUser, "object");
+  });
+});
+
+// Tests for the confirmUserLogin function
+describe("confirmUserLogin", () => {
+  it("should return errors and isValid=false if login input is invalid", async () => {
+    const data = {}; // Replace with invalid test data
+    const result = await confirmUserLogin(data);
+    expect(result.errors).toBeDefined();
+    expect(result.isValid).toBe(false);
+    expect(result.user).toBeNull();
+  });
+
+  it("should return authErrors and isValid=false if user does not exist or password is incorrect", async () => {
+    const data = {}; // Replace with valid test data
+    const result = await confirmUserLogin(data);
+    expect(result.authErrors).toBeDefined();
+    expect(result.isValid).toBe(false);
+    expect(result.user).toBeNull();
+  });
+
+  it("should return authErrors as empty and isValid=true if user login is successful", async () => {
+    const data = {}; // Replace with valid test data
+    const result = await confirmUserLogin(data);
+    expect(result.authErrors).toEqual({});
+    expect(result.isValid).toBe(true);
+    expect(result.user).toBeInstanceOf(User);
+  });
+});
+
+// Tests for the createLoginTokenAndLogin function
+describe("createLoginTokenAndLogin", () => {
+  // Please note that you will need to mock the necessary dependencies/responses to test this function
+  it("should create access token and return success response with token and user information", () => {
+    const user = {}; // Replace with valid user object
+    const res = {}; // Replace with valid response object
+
+    const spyVerify = jest.spyOn(jwt, "verify");
+    spyVerify.mockImplementation((_, __, callback) => {
+      callback(null, { decoded: true });
+    });
+
+    createLoginTokenAndLogin(user, res);
+
+    expect(spyVerify).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({
+      success: true,
+      token: expect.any(String),
+      message: user,
+    });
+
+    spyVerify.mockRestore();
+  });
+
+  it("should handle JWT verification error and return error response", () => {
+    const user = {}; // Replace with valid user object
+    const res = {}; // Replace with valid response object
+
+    const spyVerify = jest.spyOn(jwt, "verify");
+    spyVerify.mockImplementation((_, __, callback) => {
+      callback(new Error("Verification error"));
+    });
+
+    createLoginTokenAndLogin(user, res);
+
+    expect(spyVerify).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.json).toHaveBeenCalledWith({ errors: expect.any(Error) });
+
+    spyVerify.mockRestore();
+  });
+
+  it("should handle JWT decoding error and return error response", () => {
+    const user = {}; // Replace with valid user object
+    const res = {}; // Replace with valid response object
+
+    const spyVerify = jest.spyOn(jwt, "verify");
+    spyVerify.mockRejectedValue(new Error("Decoding error"));
+
+    createLoginTokenAndLogin(user, res);
+
+    expect(spyVerify).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.json).toHaveBeenCalledWith({ errors: expect.any(Error) });
+
+    spyVerify.mockRestore();
   });
 });
